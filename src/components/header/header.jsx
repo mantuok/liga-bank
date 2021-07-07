cc8ab2809c3e761c89cb34963ae7e8a00fee832e
mantuok <mantuok@gmail.com>
2021-07-08 00:14:29 +0300

    Header styling for tablet

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,7 +6,11 @@ const Header = () => {
   return (
     <header className="page__header header">
       <div className="header__container container">
-        <img className="header__logo" alt="ЛИГА Банк" src="../img/liga-logo-desktop.svg" width="150" height="27" />
+        <picture>
+          <source media="(max-width: 767px)" srcSet="../img/liga-logo-mobile.svg" />
+          <source media="(max-width: 1023px)" srcSet="../img/liga-logo-tablet.svg" />
+          <img className="header__logo" alt="ЛИГА Банк" src="../img/liga-logo-desktop.svg" width="150" height="27" />
+        </picture>
         <Navigation />
         <Link className="header__user-login user-login" to="/page-not-found">
           <span className="user-login__text">Войти в Интернет-банк</span>
