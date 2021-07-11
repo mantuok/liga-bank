83acd70713806d754a04928acf9fe3bccd60ab4e
mantuok <mantuok@gmail.com>
2021-07-11 08:43:06 +0300

    slider for desktop done

--- src/components/promo-slide/promo-slide.jsx
+++ src/components/promo-slide/promo-slide.jsx
@@ -7,13 +7,18 @@ const PromoSlide = (props) => {
 
   const getElementClass = (element) => {
     return (`${element} ${element + `--` + style}`)
-  }
+  };
+
+  const renderPromoLink = () => 
+    link === `` ? 
+    `` : 
+    <Link className={getElementClass(`promo__link`)} to={directTo}>{link}</Link>
 
   return (
     <div className="promo__container">
       <h1 className={getElementClass(`promo__header`)}>{heading}</h1>
       <p className={getElementClass(`promo__description`)}>{description}</p>
-      <Link className={getElementClass(`promo__link`)} to={directTo}>{link}</Link>
+      {renderPromoLink()}
       <img className={getElementClass(`promo__image`)} src={image} alt={alt} width="1366" height="400" />
     </div>
   )
